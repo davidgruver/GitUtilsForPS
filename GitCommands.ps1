5c3b611b560e39e3e35b5260f61da6e1f4dc7d5e
David Gruver <DavidGruver@hotmail.com>
2026-07-17 18:04:12 -0500

    Add git-is-merged function to check if a branch is merged into another

---
+++ GitCommands.ps1
@@ -0,0 +1,15 @@
+git-is-merged () {
+  merge_destination_branch=$1
+  merge_source_branch=$2
+
+  merge_base=$(git merge-base $merge_destination_branch $merge_source_branch)
+  merge_source_current_commit=$(git rev-parse $merge_source_branch)
+  if [[ $merge_base = $merge_source_current_commit ]]
+  then
+    echo $merge_source_branch is merged into $merge_destination_branch
+    return 0
+  else
+    echo $merge_source_branch is not merged into $merge_destination_branch
+    return 1
+  fi
+}
